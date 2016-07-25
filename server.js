d95fdb83fe195ca385b7d6c7b0f9b3b077a7ecbd
Ike <ikeuzo57@gmail.com>
2016-07-25 16:33:01 +0000

    initialize repo

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+var middleware = {
+    requireAuthentication: function(req, res, next){
+        console.log('private route hit!!');
+        next();
+    },
+    logger: function(req, res, next){
+        // new Date().toString()
+        console.log('Request: '  + ' ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+}
+
+
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication, function(req,res){
+    res.send('About Us!!')
+})
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(process.env.PORT,process.env.IP, function(){
+    console.log('Server is runing');
+})
